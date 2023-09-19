f7bb1971134f6a0c17a6ccca97ea61d8c73ee151
MitulRahaman <42874422+MitulRahaman@users.noreply.github.com>
2023-09-20 03:51:06 +0600

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,10 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'accountType' => ['required'],
+            'balance' => ['required'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+                           
         ]);
 
         $user = User::create([
